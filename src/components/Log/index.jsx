56ab3e8edb30df98adbde09c1fed00d5837f29db
kavery-pm <kaverypandandamadappa@Mac.fritz.box>
2025-11-06 15:25:40 +0100

    added logs

--- src/components/Log/index.jsx
+++ src/components/Log/index.jsx
@@ -1,7 +1,9 @@
-export default function Log() {
+export default function Log({ turns }) {
 
-  
-  return <ol id="log">
 
-  </ol>
+    return <ol id="log">
+        {turns.map((turn, index) =>
+            <li key={index}>Player selected ({turn.square.row}, {turn.square.col})  </li>
+        )}
+    </ol>
 }
